50e612114f78e1eb912fc60465e551dfc3c31b06
Cameron diedrich <cameron.diedrich@mths.ca>
2022-11-15 16:14:45 +0000

    change

--- greatest_number.py
+++ greatest_number.py
@@ -4,6 +4,7 @@
 # Created on: Nov 2022
 # This program finds the greatest number of three inputs
 
+
 def main():
     # Python program to find the largest number among the three input numbers
 
@@ -23,6 +24,7 @@ def main():
     print("The greatest number is", largest)
 
     print("\nDone.")
+
 
 if __name__ == "__main__":
     main()
